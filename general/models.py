713ea5af49bd2f994a3dd25d9a2faf6814ad980e
Gabriel Queiroz <gqmv@cin.ufpe.br>
2020-05-11 11:51:15 -0300

    - Disabled ForeignKeys in Appointment for Debug purposes.

--- general/models.py
+++ general/models.py
@@ -4,12 +4,12 @@ from . import presets
 
 
 class Appointment(models.Model):
-    tutor = models.ForeignKey(
-        User, null=False, on_delete=models.CASCADE, related_name="appointments_tutor"
-    )
-    student = models.ForeignKey(
-        User, null=False, on_delete=models.CASCADE, related_name="appointments_student"
-    )
+    # tutor = models.ForeignKey(
+    #     User, null=False, on_delete=models.CASCADE, related_name="appointments_tutor"
+    # )
+    # student = models.ForeignKey(
+    #     User, null=False, on_delete=models.CASCADE, related_name="appointments_student"
+    # )
     subject = models.CharField(max_length=3, choices=presets.AVAILIBLE_SUBJECTS)
     scheduled_time = models.DateTimeField()
     price = models.PositiveIntegerField()
