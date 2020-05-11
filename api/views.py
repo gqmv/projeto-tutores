19a315f88a87f03a27183ff78ef9a4afc4e03d17
Gabriel Queiroz <gqmv@cin.ufpe.br>
2020-05-11 14:05:04 -0300

    - Switched views.py to generic API views.

--- api/views.py
+++ api/views.py
@@ -1,54 +1,14 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
-from django.http import Http404
-
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import generics
 
 from general.models import Appointment
 from .serializers import AppointmentSerializer
 
 
-class Appointments(APIView):
-    def get(self, request, format=None):
-        appointments = Appointment.objects.all()
-        serializer = AppointmentSerializer(appointments, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = AppointmentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+class Appointments(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
 
 
-class AppointmentDetail(APIView):
-    def get_object(self, pk):
-        try:
-            appointment = Appointment.objects.get(id=pk)
-            return appointment
-        except Appointment.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        appointment = self.get_object(pk)
-        serializer = AppointmentSerializer(appointment)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        appointment = self.get_object(pk)
-        serializer = AppointmentSerializer(appointment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        appointment = self.get_object(pk)
-        appointment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class AppointmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
